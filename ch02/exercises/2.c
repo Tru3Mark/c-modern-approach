c5b241c9f2041dffd74dd41e7f30f59152c19e42
Tru3Mark <supermanmarcusw20@gmail.com>
2022-10-30 11:31:05 -0400

    c.2 file added

---
+++ ch02/exercises/2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Parkinson's law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
+}
+
+/*
+**
+** directive: #include <stdio.h>
+** statements: the 3 printf function calls, and the return statement
+**
+** output:
+** Parkinson's law:
+** Work expands so as to fill the time
+** available for its completion.
+**
+**
+*/
