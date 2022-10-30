acb97069a954ac0b0c3f49ffcd08137fcfcf02a6
Tru3Mark <supermanmarcusw20@gmail.com>
2022-10-30 13:52:13 -0400

    ch02 o6.c

---
+++ ch02/Projects/06.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    printf("Enter value for x for polynomial equation: ");
+    scanf("%d", &x);
+    int poly = (((((3*x + 2)*x - 5)*x - 1)*x + 7)*x-6);
+    printf("The polynomial is equal to: %d\n", poly);
+}
