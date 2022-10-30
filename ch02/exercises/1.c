30871e4faf2dca62676a35bd8bee456f0b0fae5e
Tru3Mark <supermanmarcusw20@gmail.com>
2022-10-29 23:26:50 -0400

    edited to 1.c

--- ch02/exercises/1.c
+++ ch02/exercises/1.c
@@ -4,3 +4,5 @@ int main(void)
   {
     printf("hello world\n");
   }
+
+// I do not get a warning, i think the warning would've been because no return statement, but now it is not a required feature
