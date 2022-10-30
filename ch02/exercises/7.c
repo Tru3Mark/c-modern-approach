93870f0706bee3d1b04eafaec920146c358a9b37
Tru3Mark <supermanmarcusw20@gmail.com>
2022-10-30 11:58:46 -0400

    ch02 exercises 5-7

---
+++ ch02/exercises/7.c
@@ -0,0 +1,5 @@
+/* are keywords: a(for) e(while)
+ *b(If) is not because the I is capital
+ *c(main) afaik is not a reserved keyword but it should not be used for any other case besides main function
+ *d(printf) is not a keyword, but it is a function in the standard library
+*/
