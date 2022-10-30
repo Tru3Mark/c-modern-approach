2c76b5ab2803cfcb1fc89d4127bc0090224874e1
Tru3Mark <supermanmarcusw20@gmail.com>
2022-10-30 11:48:18 -0400

    exercise 3 and 4

---
+++ ch02/exercises/4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+    int c;
+
+    float d;
+    float e;
+    float f;
+
+    printf("%d %d %d %d %d %d\n", a, b, c, d, e, f);
+    return 0;
+}
+
+// No, there is no clear pattern
